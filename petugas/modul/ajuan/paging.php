4c37ed66e645af674e88ee5fe7cd40df738765a4
AdmiralScarlet <112304081+AdmiralScarlet@users.noreply.github.com>
2025-01-31 00:21:55 +0700

    Update paging.php

--- petugas/modul/ajuan/paging.php
+++ petugas/modul/ajuan/paging.php
@@ -29,7 +29,7 @@ while ($row = mysqli_fetch_array($data_brg_in)) {
 ?>
 
     <tr>
-
+        <td><?php echo $nomor++ ?></td>
         <td><?php echo $row['no_ajuan']; ?></td>
         <td><?php echo $row['tanggal']; ?></td>
         <td><?php echo $row['kode_brg']; ?></td>
